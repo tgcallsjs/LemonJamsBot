ee0d03d7471e2bd03a99e24422aa357d2d548edb
Roj Serbest <rojserbest@gmail.com>
2021-07-08 13:44:59 +0300

    Add user logged in message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Api } from 'telegram';
 import { client } from './client';
 import { bot } from './bot';
 import { initHandlers } from './handlers';
@@ -6,6 +7,9 @@ import { initHandlers } from './handlers';
     initHandlers();
 
     await client.start({ botAuthToken: '' });
+    const me = await client.getMe() as Api.User;
+    console.log('Logged in as ', me?.firstName);
+
     await bot.launch();
     console.log(`@${bot.botInfo?.username} is running...`);
 })();
